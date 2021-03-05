d380a4ff9f47814fe9073703d3f67d4d4fe13feb
Oliver Batchelor <oliver.batchelor@canterbury.ac.nz>
2021-03-05 18:37:44 +1300

    Add ability to write out board configs to files

--- multical/app/check_boards.py
+++ multical/app/check_boards.py
@@ -16,23 +16,27 @@ from multical.image.detect import load_image
 def show_boards(args):
   board_file = args.boards
   boards = board.load_config(board_file)
-  
- 
+
   print("Using boards:")
   for name, b in boards.items():
     print(f"{name} {b}")
 
   image_size = None
   if args.image_size is not None:
-    image_size = args.image_size.split('x')
+    image_size = [int(x) for x in args.image_size.split('x')]
     assert len(image_size) == 2, "expected WxH image_size e.g. 800x600"
+
 
   def draw_board(board):
     board_image = board.draw(args.square_length, args.margin)
 
     if image_size is not None:
-      image = np.full((image_size[1], image_size[0]), 255)
-      dy, dx = [(a - b) // 2  for a, b in zip(board_image.shape, image.shape)]
+      image = np.full((image_size[1], image_size[0]), 255, dtype=np.uint8)
+      dy, dx = [(a - b) // 2  for a, b in zip(image.shape, board_image.shape)]
+
+      assert dx >= 0 and dy >= 0,\
+        f"--image_size ({args.image_size}) must be larger than board image ({board_image.shape[1]}x{board_image.shape[0]})"
+
       image[dy:dy+board_image.shape[0], dx:dx + board_image.shape[1]] = board_image
       return image
 

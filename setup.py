d5d5fbb19dcd0a6a0c1fafc92a7cc158ce7fe573
Oliver Batchelor <saulzar@gmail.com>
2021-04-07 17:25:44 +1200

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
 
     name="multical",
-    version="0.1.2",
+    version="0.1.3",
     author="Oliver Batchelor",
     author_email="saulzar@gmail.com",
     description="Flexible multi-camera multi-board camera calibration library and application.",

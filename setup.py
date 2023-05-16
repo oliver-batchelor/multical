cc3bf8e3634839fb7f83380ab8e1ede72063d312
Oliver Batchelor <saulzar@gmail.com>
2023-05-16 19:51:53 +1200

    Update setup.py

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
         "simple-parsing",
         "natsort",
         "cached-property",
-        "py-structs<=1.0",
+        "py-structs<1.0",
         "palettable",
         "numpy-quaternion",
         "omegaconf",
